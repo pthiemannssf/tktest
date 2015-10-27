4d0638c34653f360e7d5269643854442ad00cbda
pthiemannssf <pthiemann@softstackfactory.org>
2015-10-27 23:36:50 +0000

    gulpfile update

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var Server = require('karma').Server;
 gulp.task('test', function(done) {
   new Server({
-    configFile: __dirname + '/tests/my.conf.js',
+    configFile: __dirname + '/tktest/tests/my.conf.js',
     singleRun: true,
     autoWatch: false
   }, done).start();
